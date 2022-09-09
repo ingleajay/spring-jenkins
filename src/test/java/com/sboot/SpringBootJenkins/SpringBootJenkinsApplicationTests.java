0c0b659560c795bb736588ef3eb60a3cd792600e
Ajay Ingle <ajay.ingle@vit.edu.in>
2022-09-09 16:34:52 +0530

    jenkins

---
+++ src/test/java/com/sboot/SpringBootJenkins/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package com.sboot.SpringBootJenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplicationTests.class);
+
+	
+	@Test
+	void contextLoads() {
+		logger.info("Application tested...");
+		assertEquals(true, true);
+	}
+
+}
